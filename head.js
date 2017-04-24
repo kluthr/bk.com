2a8e02246b73a50acbd769b51440a1c0036677cf
kluthr <becky@trapit.com>
2017-04-23 22:09:38 -0700

    Change all the things

---
+++ head.js
@@ -0,0 +1,1 @@
+$("#home").headroom(),$("img.lazy").lazyload({effect:"fadeIn"}),$(window).scroll(function(){var o=$(window).scrollTop();o>=5?$("header").removeClass("nav-root"):6>=o&&$("header").addClass("nav-root")});
